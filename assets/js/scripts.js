bb579da08888e46f38540761d8bf88a531d0ec59
C-ESTIBAR5 <cce7990@students.uc-bcf.edu.ph>
2024-02-10 19:08:49 +0800

    added functions for nav bar

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -8,6 +8,15 @@ document.querySelector('.menu-icons.open').addEventListener('click', function() 
   document.querySelector('.menu-icons.close').addEventListener('click', function() {
     document.querySelector('.menu-icons.close').style.display = 'none';
     document.querySelector('.menu-icons.open').style.display = 'block';
-
   });
   
+
+  let popup = document.getElementById("popup");
+
+  function openPopup() {
+    popup.classList.add("open-popup");
+  }
+
+  function closePopup() {
+    popup.classList.remove("close-popup");
+  }
